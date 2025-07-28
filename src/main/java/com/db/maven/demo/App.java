8a1e5e813278ec0b34075f9f76faaf0804ef3ee2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 15:05:07 +0530

    maven demo

---
+++ src/main/java/com/db/maven/demo/App.java
@@ -0,0 +1,13 @@
+package com.db.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
