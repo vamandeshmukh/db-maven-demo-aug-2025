9d5710d98f7cade53f88f91674b58a978a7dd583
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 16:07:10 +0530

    edited pom.xml

--- db.sql
+++ db.sql
@@ -1,11 +1,14 @@
 
 SELECT * FROM employees;
 
-
 CREATE TABLE employees (id SERIAL PRIMARY KEY, name VARCHAR(50),salary NUMERIC(10, 2));
+
 
 INSERT INTO employees (id, name, salary) VALUES (101, 'Sonu', 90000);
 INSERT INTO employees (id, name, salary) VALUES (102, 'Monu', 95000);
 INSERT INTO employees (id, name, salary) VALUES (103, 'Tonu', 88000);
 INSERT INTO employees (id, name, salary) VALUES (104, 'Ponu', 92000);
 
+COMMIT; 
+
+SELECT * FROM employees;
