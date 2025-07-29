ee100a4db5f1597357cad5a4e6dc2df782b132d7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-29 10:00:10 +0530

    assgn code

--- src/test/java/com/db/maven/demo/service/EmployeeServiceTest.java
+++ src/test/java/com/db/maven/demo/service/EmployeeServiceTest.java
@@ -1,6 +1,39 @@
 package com.db.maven.demo.service;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class EmployeeServiceTest {
 
+
+
+
 }
+
+
+//package com.db.maven.demo.service;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//
+//class EmployeeServiceTest {
+//
+//    @Test
+//    void testTest() {
+//        int expected = 10;
+//        int actual = 5 + 5;
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void testTest2() {
+//        int expected = 10;
+//        int actual = 5 + 6;
+//        assertNotEquals(expected, actual);
+//    }
+//}
+//
